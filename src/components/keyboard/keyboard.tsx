24452123b4ca4aed73dd23835208cb482ccd9d1c
Gil Zausmer <gil@brandtotal.com>
2021-01-21 15:21:18 +0200

    first commit

---
+++ src/components/keyboard/keyboard.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./keyboard.scss";
+export function Keyboard() {
+  return (
+    <ul className="key_wrapper">
+      <li className="white b" />
+      <li className="black as" />
+      <li className="white a" />
+      <li className="black gs" />
+      <li className="white g" />
+      <li className="black fs" />
+      <li className="white f" />
+      <li className="white e" />
+      <li className="black ds" />
+      <li className="white d" />
+      <li className="black cs" />
+      <li className="white c" />
+    </ul>
+  );
+}
