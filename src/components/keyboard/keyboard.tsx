e3285dcde1cf0e0f3623dffbef24faba79fcea30
Gil Zausmer <gil@brandtotal.com>
2021-01-23 17:30:59 +0200

    add colors on hover

--- src/components/keyboard/keyboard.tsx
+++ src/components/keyboard/keyboard.tsx
@@ -1,20 +1,37 @@
 import React from "react";
-import "./keyboard.scss";
+import styles from "./keyboard.module.scss";
+import { useAuth0 } from "@auth0/auth0-react";
+import classNames from "classnames";
+
 export function Keyboard() {
+  const { user } = useAuth0();
+  console.log({ user });
+
   return (
-    <ul className="key_wrapper">
-      <li className="white b" />
-      <li className="black as" />
-      <li className="white a" />
-      <li className="black gs" />
-      <li className="white g" />
-      <li className="black fs" />
-      <li className="white f" />
-      <li className="white e" />
-      <li className="black ds" />
-      <li className="white d" />
-      <li className="black cs" />
-      <li className="white c" />
-    </ul>
+    <div>
+      {user?.picture && (
+        <div>
+          <img
+            src={user?.picture}
+            alt={"no"}
+            className={styles.profile_container}
+          />
+        </div>
+      )}
+      <ul className={styles.key_wrapper}>
+        <li className={styles.white} />
+        <li className={styles.black} />
+        <li className={classNames(styles.white, styles.add_margin)} />
+        <li className={styles.black} />
+        <li className={classNames(styles.white, styles.add_margin)} />
+        <li className={styles.black} />
+        <li className={classNames(styles.white, styles.add_margin)} />
+        <li className={styles.white} />
+        <li className={styles.black} />
+        <li className={classNames(styles.white, styles.add_margin)} />
+        <li className={styles.black} />
+        <li className={classNames(styles.white, styles.add_margin)} />
+      </ul>
+    </div>
   );
 }
