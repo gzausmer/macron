3a51573783cd4d5eb80302193eccfa8c79269986
Gil Zausmer <gil@brandtotal.com>
2021-01-23 22:31:22 +0200

    rename scss file to module.scss add classnames

--- src/components/keyboard/keyboard.tsx
+++ src/components/keyboard/keyboard.tsx
@@ -1,23 +1,10 @@
 import React from "react";
 import styles from "./keyboard.module.scss";
-import { useAuth0 } from "@auth0/auth0-react";
 import classNames from "classnames";
 
 export function Keyboard() {
-  const { user } = useAuth0();
-  console.log({ user });
-
   return (
     <div>
-      {user?.picture && (
-        <div>
-          <img
-            src={user?.picture}
-            alt={"no"}
-            className={styles.profile_container}
-          />
-        </div>
-      )}
       <ul className={styles.key_wrapper}>
         <li className={styles.white} />
         <li className={styles.black} />
