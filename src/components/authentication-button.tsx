d3b8833dadaf7d996f454eb9183a33baae5cf0b2
Gil Zausmer <gil@brandtotal.com>
2021-01-23 17:13:09 +0200

    add auth0

---
+++ src/components/authentication-button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { LogoutButton } from "./logout-button";
+import { SignupButton } from "./signUp-button";
+
+export const AuthenticationButton = () => {
+  const { isAuthenticated } = useAuth0();
+
+  return isAuthenticated ? <LogoutButton /> : <SignupButton />;
+};
